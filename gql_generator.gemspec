2d1fe9874d83d4e4b599e0a59025330ef2a92506
Clinton Mbah <clintonmbah44@gmail.com>
2019-10-01 15:16:48 +0000

    initial commit

---
+++ gql_generator.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = "gql-generator"
+  s.version     = "0.1"
+  s.platform    = "ruby"
+  s.authors     = ["Clinton Mbah"]
+  s.email       = ["clintonmbah44@gmail.com"]
+  s.homepage    = "https://github.com/mclintproject/gql-generator"
+  s.summary     = "A better Rails generator for GraphQL Ruby."
+  s.description = "This generator will create types, mutations and their respective test files for you."
+  s.files = Dir.glob("{lib}/**/*")
+  s.require_path = 'lib'
+  s.add_development_dependency 'rails', '~> 6.0'
+end
---
+++ lib/generator/gql_generator.rb
@@ -0,0 +1,99 @@
+require 'rails/generators/base'
+
+class GqlGenerator < Rails::Generators::Base
+  source_root File.expand_path("templates", __dir__)
+  argument :name, type: :string, required: true
+  class_option :arguments, type: :array, required: true, default: []
+  class_option :fields, type: :array, required: true, default: []
+  class_option :type, type: :string, default: "mutation"
+  
+  def generate
+    case options[:type]
+    when "mutation"
+      generate_mutation_files
+    when "object"
+      generate_type_files
+    when "input"
+      generate_input_file
+    end
+  end
+
+  private
+
+  TYPES = {
+    id: "ID",
+    int: "Int",
+    string: "String",
+    float: "Float",
+    bool: "Boolean",
+    date: "GraphQL::Types::ISO8601Date",
+    datetime: "GraphQL::Types::ISO8601DateTime",
+    json: "GraphQL::Types::JSON",
+  }.freeze
+
+  def generate_mutation_files
+    if required_params_present :fields, :arguments
+      template "mutation_file.template",
+        "app/graphql/mutations/#{name}.rb"
+      template "mutation_spec.template",
+        "spec/graphql/mutations/#{name}_spec.rb"
+    end
+  end
+
+  def generate_type_files
+    if required_params_present :fields
+      template "type_file.template",
+        "app/graphql/types/#{name}_type.rb"
+      template "type_spec.template",
+        "spec/graphql/types/#{name}_type_spec.rb"
+    end
+  end
+
+  def generate_input_file
+    if required_params_present :fields
+      template "input_file.template",
+        "app/graphql/input/#{name}_input.rb"
+    end
+  end
+
+  def arguments
+    options[:arguments].map do |argument|
+      argument_options = argument.split(":")
+      OpenStruct.new(
+        name: argument_options[0],
+        type: type_of(argument_options[1]),
+        required: argument_options[2] || "false"
+      )
+    end
+  end
+
+  def fields
+    options[:fields].map do |field|
+        field_options = field.split(":")
+        OpenStruct.new(
+          name: field_options[0],
+          type: type_of(field_options[1]),
+          null: field_options[2] || "false"
+        )
+      end
+  end
+
+  def type_of(type)
+    type = type ||= "string"
+    TYPES.fetch(type.to_sym, "String")
+  end
+
+  def ensure_required_params_present(*required_params)
+    required_params_present = true
+
+    required_params.each do |param|
+      if send(param).empty?
+        puts "'#{param.to_s}' parameter is required."
+        required_params_present = false
+      end
+    end
+
+    required_params_present
+  end
+end
+  
